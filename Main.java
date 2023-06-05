279bf10d858c0ba43c542ce8506feb2835ecba86
ErichHans <rommelerwin777@gmail.com>
2023-06-05 20:24:17 +0700

    update

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import javax.swing.*;
 import java.net.*;
 import java.io.*;
 import java.util.*;
@@ -13,7 +11,7 @@ public class Main extends JFrame implements ActionListener {
     private static final String TERMINATE = "Exit";
     static String name;
     static volatile boolean finished = false;
-    InetAddress group = InetAddress.getByName("224.0.1.255");
+    InetAddress group = InetAddress.getByName("127.0.1");
     int port = Integer.parseInt("1234");
     MulticastSocket socket = new MulticastSocket(port);
     
@@ -31,11 +29,7 @@ public class Main extends JFrame implements ActionListener {
         
         // Tạo ra nút Send
         sendButton = new JButton("gửi tin nhắn");
-        sendButton.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent ae) {
-            sendMessage();
-          }
-        });
+        sendButton.addActionListener(this);
         setPreferredSize(new Dimension(80, 30));
         add(sendButton, BorderLayout.EAST);
 
@@ -75,100 +69,52 @@ public class Main extends JFrame implements ActionListener {
                 e1.printStackTrace();
             }
         }
-      }
-    });
-        try {
+        }
+    public static void main(String[] args) throws IOException {
+        Main groupchat=new Main();
         System.out.print("Enter Name:");
         Scanner sc = new Scanner(System.in);
         name = sc.nextLine();
         sc.close();
-          input = new BufferedReader(new InputStreamReader(server.getInputStream()));
-          output = new PrintWriter(server.getOutputStream(), true);
-          output.println(name);
-          read = new Read();
-          read.start();
-        } catch (Exception ex) {
-          JOptionPane.showMessageDialog(jfr, ex.getMessage());
+    }
+    public void addtoma(String message){
+        messageArea.append(message);
+    }
+}
+class ReadThread implements Runnable
+{
+    private MulticastSocket socket;
+    private InetAddress group;
+    private int port;
+    private static final int MAX_LEN = 1000;
+    ReadThread(MulticastSocket socket,InetAddress group,int port)
+    {
+        this.socket = socket;
+        this.group = group;
+        this.port = port;
+    }
+      
+    @Override
+    public void run()
+    {
+        while(!Main.finished)
+        {
+                byte[] buffer = new byte[ReadThread.MAX_LEN];
+                DatagramPacket datagram = new
+                DatagramPacket(buffer,buffer.length,group,port);
+                String message;
+            try
+            {
+                socket.receive(datagram);
+                message = new
+                String(buffer,0,datagram.getLength(),"UTF-8");
+                if(!message.startsWith(Main.name))
+                    Main.messageArea.append(message);
+            }
+            catch(IOException e)
+            {
+                System.out.println("May chu da dong cua!");
+            }
         }
-  }
-  public class TextListener implements DocumentListener{
-    JTextField jtf1;
-    JTextField jtf2;
-    JTextField jtf3;
-    JButton jcbtn;
-
-    public TextListener(JTextField jtf1, JTextField jtf2, JTextField jtf3, JButton jcbtn){
-      this.jtf1 = jtf1;
-      this.jtf2 = jtf2;
-      this.jtf3 = jtf3;
-      this.jcbtn = jcbtn;
     }
-
-    public void changedUpdate(DocumentEvent e) {}
-
-    public void removeUpdate(DocumentEvent e) {
-      if(jtf1.getText().trim().equals("") ||
-          jtf2.getText().trim().equals("") ||
-          jtf3.getText().trim().equals("")
-          ){
-        jcbtn.setEnabled(false);
-      }else{
-        jcbtn.setEnabled(true);
-      }
-    }
-    public void insertUpdate(DocumentEvent e) {
-      if(jtf1.getText().trim().equals("") ||
-          jtf2.getText().trim().equals("") ||
-          jtf3.getText().trim().equals("")
-          ){
-        jcbtn.setEnabled(false);
-      }else{
-        jcbtn.setEnabled(true);
-      }
-    }
-
-  }
-  public void sendMessage() {
-    if(name!=null){
-    try {
-      String message = messageField.getText();
-      if (message.equals("")) {
-        return;
-      }
-      output.println(message);
-      messageField.requestFocus();
-      messageField.setText(null);
-    } catch (Exception ex) {
-      JOptionPane.showMessageDialog(null, ex.getMessage());
-      System.exit(0);
-    }
-  }else{
-    messageArea.append("Vui Lòng Nhập Tên Của Bạn...");
-    messageField.requestFocus();
-    messageField.setText(null);
-  }
-  }
-
-  public static void main(String[] args) throws Exception {
-    Main client = new Main();
-  }
-  class Read extends Thread {
-    public void run() {
-      String message;
-      while(!Thread.currentThread().isInterrupted()){
-        try {
-          message = input.readLine();
-          if(message != null){
-            messageArea.append(message+"\n");
-        }}
-        catch (IOException ex) {
-          System.err.println("Failed to parse incoming message");
-        }
-      }
-    }
-  }
-
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-  }}
+}
