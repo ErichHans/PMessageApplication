d83ccdde72a52927e2f3fe803e2678409b1fa719
ErichHans <rommelerwin777@gmail.com>
2023-06-06 05:20:51 +0700

    Update

--- Main.java
+++ Main.java
@@ -1,170 +1,66 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import javax.swing.*;
-import javax.swing.text.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.html.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.awt.BorderLayout;
+import java.awt.event.*;
 
+public class Main extends JFrame implements ActionListener {
+    private JTextField textField;
+    private JTextArea textArea;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
 
-public class Main extends JFrame implements ActionListener{
-  private String oldMsg = "";
-  private Thread read;
-  private String serverName;
-  private int PORT;
-  BufferedReader input;
-  PrintWriter output;
-  Socket server;
-  private JTextField messageField;
-  private JButton sendButton;
-  public static JTextArea messageArea;
-  private static final String TERMINATE = "Exit";
-  static String name;
-  static volatile boolean finished = false;
-
-  public Main() throws UnknownHostException, IOException {
-    this.serverName = "192.168.1.138";
-    
-    this.PORT = 12345;
-    server = new Socket(serverName, PORT);
-
-    final JFrame jfr = new JFrame("Chat");
-    setTitle("PMessage");
-        setSize(1280, 720);
-        setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
-        setLayout(new BorderLayout());
-
-        // tạo ra nơi để nhập tin nhắn
-        messageField = new JTextField();
-        add(messageField, BorderLayout.SOUTH);
-        
-        // Tạo ra nút Send
-        sendButton = new JButton("gửi tin nhắn");
-        sendButton.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent ae) {
-            sendMessage();
-          }
-        });
-        setPreferredSize(new Dimension(80, 30));
-        add(sendButton, BorderLayout.EAST);
-
-        // Khu vực hiển thị tin nhắn
-        messageArea = new JTextArea();
-        messageArea.setEditable(false);
-        add(messageArea, BorderLayout.CENTER);
-
-        // Cửa sổ hiện ra và có thể tắt khi bấm thoát
-        setVisible(true);
+    public Main(String serverAddress) throws IOException {
+        super("PMessage");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+  
 
-    messageField.addKeyListener(new KeyAdapter() {
-      public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-          sendMessage();
+        textField = new JTextField();
+        textField.addActionListener(this);
+        add(textField, BorderLayout.SOUTH);
+
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        socket = new Socket( serverAddress, 11087);
+        in = new BufferedReader(new InputStreamReader(
+            socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    while (true) {
+                        String line = in.readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        textArea.append(line + "\n");
+                    }
+                } catch (IOException e) {
+                    textArea.append("Error: " + e + "\n");
+                }
+            }
+        }).start();
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String text = textField.getText();
+        out.println(text);
+        textField.setText("");
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: java SimpleChat <server>");
+            return;
         }
-      }
-    });
-        try {
-        System.out.print("Enter Name:");
-        Scanner sc = new Scanner(System.in);
-        name = sc.nextLine();
-        sc.close();
-          input = new BufferedReader(new InputStreamReader(server.getInputStream()));
-          output = new PrintWriter(server.getOutputStream(), true);
-          output.println(name);
-          read = new Read();
-          read.start();
-        } catch (Exception ex) {
-          JOptionPane.showMessageDialog(jfr, ex.getMessage());
-        }
-  }
-  public class TextListener implements DocumentListener{
-    JTextField jtf1;
-    JTextField jtf2;
-    JTextField jtf3;
-    JButton jcbtn;
-
-    public TextListener(JTextField jtf1, JTextField jtf2, JTextField jtf3, JButton jcbtn){
-      this.jtf1 = jtf1;
-      this.jtf2 = jtf2;
-      this.jtf3 = jtf3;
-      this.jcbtn = jcbtn;
+        new Main(args[0]);
     }
-
-    public void changedUpdate(DocumentEvent e) {}
-
-    public void removeUpdate(DocumentEvent e) {
-      if(jtf1.getText().trim().equals("") ||
-          jtf2.getText().trim().equals("") ||
-          jtf3.getText().trim().equals("")
-          ){
-        jcbtn.setEnabled(false);
-      }else{
-        jcbtn.setEnabled(true);
-      }
-    }
-    public void insertUpdate(DocumentEvent e) {
-      if(jtf1.getText().trim().equals("") ||
-          jtf2.getText().trim().equals("") ||
-          jtf3.getText().trim().equals("")
-          ){
-        jcbtn.setEnabled(false);
-      }else{
-        jcbtn.setEnabled(true);
-      }
-    }
-
-  }
-  public void sendMessage() {
-    if(name!=null){
-    try {
-      String message = messageField.getText();
-      if (message.equals("")) {
-        return;
-      }
-      output.println(message);
-      messageField.requestFocus();
-      messageField.setText(null);
-    } catch (Exception ex) {
-      JOptionPane.showMessageDialog(null, ex.getMessage());
-      System.exit(0);
-    }
-  }else{
-    messageArea.append("Vui Lòng Nhập Tên Của Bạn...");
-    messageField.requestFocus();
-    messageField.setText(null);
-  }
-  }
-
-  public static void main(String[] args) throws Exception {
-    Main client = new Main();
-  }
-  class Read extends Thread {
-    public void run() {
-      String message;
-      while(!Thread.currentThread().isInterrupted()){
-        try {
-          message = input.readLine();
-          if(message != null){
-            messageArea.append(message+"\n");
-        }}
-        catch (IOException ex) {
-          System.err.println("Failed to parse incoming message");
-        }
-      }
-    }
-  }
-
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-  }}
+}
--- Server.java
+++ Server.java
@@ -1,170 +1,78 @@
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.awt.Color;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Server {
+    private static Set<String> names = new HashSet<>();
+    private static Set<PrintWriter> writers = new HashSet<>();
 
-  private int port;
-  private List<User> clients;
-  private ServerSocket server;
-
-  public static void main(String[] args) throws IOException {
-    new Server(12345).run();
-  }
-
-  public Server(int port) {
-    this.port = port;
-    this.clients = new ArrayList<User>();
-  }
-
-  public void run() throws IOException {
-    server = new ServerSocket(port) {
-      protected void finalize() throws IOException {
-        this.close();
-      }
-    };
-    System.out.println("Port 12345 is now open.");
-
-    while (true) {
-      try{
-      // accepts a new client
-      Socket client = server.accept();
-
-      // get nickname of newUser
-      String nickname = (new Scanner ( client.getInputStream())).nextLine();
-      nickname = nickname.replace(",", ""); //  ',' use for serialisation
-      nickname = nickname.replace(" ", "_");
-      System.out.println("New Client: \"" + nickname + "\"\n\t     Host:" + client.getInetAddress().getHostAddress());
-      
-      // create new User
-      User newUser = new User(client, nickname);
-      
-      // add newUser message to list
-      this.clients.add(newUser);
-      
-      // create a new thread for newUser incoming messages handling
-      new Thread(new UserHandler(this, newUser)).start();
+    public static void main(String[] args) throws Exception {
+        System.out.println("The chat server is running...");
+        ServerSocket listener = new ServerSocket(11087);
+        try {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
     }
-    catch(Exception e){
 
-    }
-    }
-  }
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
 
-  // delete a user from the list
-  public void removeUser(User user){
-    this.clients.remove(user);
-  }
-
-  // send incoming msg to all Users
-  public void broadcastMessages(String msg, User userSender) {
-    for (User client : this.clients) {
-      client.getOutStream().println(
-          userSender.toString() + " : "+msg);
-    }
-  }
-
-  public void sendMessageToUser(String msg, User userSender, String user){;
-    for (User client : this.clients) {
-      if (client.getNickname().equals(user) && client != userSender) {
-        userSender.getOutStream().println(userSender.toString() + " -> " + client.toString() +": " + msg);
-        client.getOutStream().println(
-            "(<b>Private</b>)" + userSender.toString() + msg);
-      }
-    }
-  }
-}
-
-class UserHandler implements Runnable {
-
-  private Server server;
-  private User user;
-
-  public UserHandler(Server server, User user) {
-    this.server = server;
-    this.user = user;
-  }
-
-  public void run() {
-    String message;
-
-    // when there is a new message, broadcast to all
-    Scanner sc = new Scanner(this.user.getInputStream());
-    while (sc.hasNextLine()) {
-      message = sc.nextLine();
-
-      // Gestion des messages private
-      if (message.charAt(0) == '@'){
-        if(message.contains(" ")){
-          System.out.println("private msg : " + message);
-          int firstSpace = message.indexOf(" ");
-          String userPrivate= message.substring(1, firstSpace);
-          server.sendMessageToUser(
-              message.substring(
-                firstSpace+1, message.length()
-                ), user, userPrivate
-              );
+        public Handler(Socket socket) {
+            this.socket = socket;
         }
 
-      // Gestion du changement
-      }else if (message.charAt(0) == '#'){
-        // update color for all other users
-      }else{
-        // update user list
-        server.broadcastMessages(message, user);
-      }
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.readLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!names.contains(name)) {
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+
+                out.println("NAMEACCEPTED");
+                writers.add(out);
+
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + name + ": " + input);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                if (name != null) {
+                    names.remove(name);
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {}
+            }
+        }
     }
-    // end of Thread
-    server.removeUser(user);
-    sc.close();
-  }
-}
-
-class User {
-  private static int nbUser = 0;
-  private int userId;
-  private PrintStream streamOut;
-  private InputStream streamIn;
-  private String nickname;
-  private Socket client;
-  private String color;
-
-  // constructor
-  public User(Socket client, String name) throws IOException {
-    this.streamOut = new PrintStream(client.getOutputStream());
-    this.streamIn = client.getInputStream();
-    this.client = client;
-    this.nickname = name;
-    this.userId = nbUser;
-    nbUser += 1;
-  }
-
-  // getteur
-  public PrintStream getOutStream(){
-    return this.streamOut;
-  }
-
-  public InputStream getInputStream(){
-    return this.streamIn;
-  }
-
-  public String getNickname(){
-    return this.nickname;
-  }
-
-  // print user with his color
-  public String toString(){
-
-    return this.getNickname();
-
-  }
 }
