b2fdb49affb18ed5cd45fc620debfb63b9064487
HoangQuanGITHUB <caohoangquannguyen@gmail.com>
2023-06-05 15:45:05 +0700

    Multicast -> IPv4

--- Main.java
+++ Main.java
@@ -1,24 +1,43 @@
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.net.*;
 import java.io.*;
-import java.util.*;
-public class Main extends JFrame implements ActionListener {
-    private JTextField messageField;
-    private JButton sendButton;
-    public static JTextArea messageArea;
-    private static final String TERMINATE = "Exit";
-    static String name;
-    static volatile boolean finished = false;
-    InetAddress group = InetAddress.getByName("224.0.1.255");
-    int port = Integer.parseInt("1234");
-    MulticastSocket socket = new MulticastSocket(port);
-    
-    public Main() throws IOException {
-      
-        // tạo ra giao diện cửa sổ
-        setTitle("PMessage");
+import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.html.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main extends JFrame implements ActionListener{
+  private String oldMsg = "";
+  private Thread read;
+  private String serverName;
+  private int PORT;
+  BufferedReader input;
+  PrintWriter output;
+  Socket server;
+  private JTextField messageField;
+  private JButton sendButton;
+  public static JTextArea messageArea;
+  private static final String TERMINATE = "Exit";
+  static String name;
+  static volatile boolean finished = false;
+
+  public Main() throws UnknownHostException, IOException {
+    this.serverName = "localhost";
+    this.PORT = 12345;
+    server = new Socket(serverName, PORT);
+
+    final JFrame jfr = new JFrame("Chat");
+    setTitle("PMessage");
         setSize(1280, 720);
         setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
         setLayout(new BorderLayout());
@@ -29,7 +48,11 @@ public class Main extends JFrame implements ActionListener {
         
         // Tạo ra nút Send
         sendButton = new JButton("gửi tin nhắn");
-        sendButton.addActionListener(this);
+        sendButton.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent ae) {
+            sendMessage();
+          }
+        });
         setPreferredSize(new Dimension(80, 30));
         add(sendButton, BorderLayout.EAST);
 
@@ -41,80 +64,106 @@ public class Main extends JFrame implements ActionListener {
         // Cửa sổ hiện ra và có thể tắt khi bấm thoát
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        socket.setTimeToLive(0);
-       
-          
-        socket.joinGroup(group);
-        Thread t = new Thread(new
-        ReadThread(socket,group,port));
-        
-        
-        t.start();
-        
-    }
-    public void sendMessage(String message) throws IOException{
-    message = name + ": " + message+"\n";
-    messageArea.append(message);
-    byte[] buffer = message.getBytes();
-    DatagramPacket datagram = new DatagramPacket(buffer,buffer.length,group,port);
-    socket.send(datagram);
-    }
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource() == sendButton) {
-            String mess = messageField.getText();
-            messageField.setText("");
-            try {
-                sendMessage(mess);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+
+    messageField.addKeyListener(new KeyAdapter() {
+      public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+          sendMessage();
         }
-        }
-    public static void main(String[] args) throws IOException {
-        Main groupchat=new Main();
+      }
+    });
+        try {
         System.out.print("Enter Name:");
         Scanner sc = new Scanner(System.in);
         name = sc.nextLine();
         sc.close();
-    }
-    public void addtoma(String message){
-        messageArea.append(message);
-    }
-}
-class ReadThread implements Runnable
-{
-    private MulticastSocket socket;
-    private InetAddress group;
-    private int port;
-    private static final int MAX_LEN = 1000;
-    ReadThread(MulticastSocket socket,InetAddress group,int port)
-    {
-        this.socket = socket;
-        this.group = group;
-        this.port = port;
-    }
-      
-    @Override
-    public void run()
-    {
-        while(!Main.finished)
-        {
-                byte[] buffer = new byte[ReadThread.MAX_LEN];
-                DatagramPacket datagram = new
-                DatagramPacket(buffer,buffer.length,group,port);
-                String message;
-            try
-            {
-                socket.receive(datagram);
-                message = new
-                String(buffer,0,datagram.getLength(),"UTF-8");
-                if(!message.startsWith(Main.name))
-                    Main.messageArea.append(message);
-            }
-            catch(IOException e)
-            {
-                System.out.println("May chu da dong cua!");
-            }
+          input = new BufferedReader(new InputStreamReader(server.getInputStream()));
+          output = new PrintWriter(server.getOutputStream(), true);
+          output.println(name);
+          read = new Read();
+          read.start();
+        } catch (Exception ex) {
+          JOptionPane.showMessageDialog(jfr, ex.getMessage());
         }
+  }
+  public class TextListener implements DocumentListener{
+    JTextField jtf1;
+    JTextField jtf2;
+    JTextField jtf3;
+    JButton jcbtn;
+
+    public TextListener(JTextField jtf1, JTextField jtf2, JTextField jtf3, JButton jcbtn){
+      this.jtf1 = jtf1;
+      this.jtf2 = jtf2;
+      this.jtf3 = jtf3;
+      this.jcbtn = jcbtn;
     }
-}
+
+    public void changedUpdate(DocumentEvent e) {}
+
+    public void removeUpdate(DocumentEvent e) {
+      if(jtf1.getText().trim().equals("") ||
+          jtf2.getText().trim().equals("") ||
+          jtf3.getText().trim().equals("")
+          ){
+        jcbtn.setEnabled(false);
+      }else{
+        jcbtn.setEnabled(true);
+      }
+    }
+    public void insertUpdate(DocumentEvent e) {
+      if(jtf1.getText().trim().equals("") ||
+          jtf2.getText().trim().equals("") ||
+          jtf3.getText().trim().equals("")
+          ){
+        jcbtn.setEnabled(false);
+      }else{
+        jcbtn.setEnabled(true);
+      }
+    }
+
+  }
+  public void sendMessage() {
+    if(name!=null){
+    try {
+      String message = messageField.getText();
+      if (message.equals("")) {
+        return;
+      }
+      output.println(message);
+      messageField.requestFocus();
+      messageField.setText(null);
+    } catch (Exception ex) {
+      JOptionPane.showMessageDialog(null, ex.getMessage());
+      System.exit(0);
+    }
+  }else{
+    messageArea.append("Vui Lòng Nhập Tên Của Bạn...");
+    messageField.requestFocus();
+    messageField.setText(null);
+  }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Main client = new Main();
+  }
+  class Read extends Thread {
+    public void run() {
+      String message;
+      while(!Thread.currentThread().isInterrupted()){
+        try {
+          message = input.readLine();
+          if(message != null){
+            messageArea.append(message+"\n");
+        }}
+        catch (IOException ex) {
+          System.err.println("Failed to parse incoming message");
+        }
+      }
+    }
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+  }}
--- Server.java
+++ Server.java
@@ -35,30 +35,28 @@ public class Server {
     System.out.println("Port 12345 is now open.");
 
     while (true) {
+      try{
       // accepts a new client
       Socket client = server.accept();
 
       // get nickname of newUser
-      String nickname = (new Scanner ( client.getInputStream() )).nextLine();
+      String nickname = (new Scanner ( client.getInputStream())).nextLine();
       nickname = nickname.replace(",", ""); //  ',' use for serialisation
       nickname = nickname.replace(" ", "_");
       System.out.println("New Client: \"" + nickname + "\"\n\t     Host:" + client.getInetAddress().getHostAddress());
-
+      
       // create new User
       User newUser = new User(client, nickname);
-
+      
       // add newUser message to list
       this.clients.add(newUser);
-
-      // Welcome msg
-      newUser.getOutStream().println(
-          "<img src='https://www.kizoa.fr/img/e8nZC.gif' height='42' width='42'>"
-          + "<b>Welcome</b> " + newUser.toString() +
-          "<img src='https://www.kizoa.fr/img/e8nZC.gif' height='42' width='42'>"
-          );
-
+      
       // create a new thread for newUser incoming messages handling
       new Thread(new UserHandler(this, newUser)).start();
+    }
+    catch(Exception e){
+
+    }
     }
   }
 
@@ -71,30 +69,17 @@ public class Server {
   public void broadcastMessages(String msg, User userSender) {
     for (User client : this.clients) {
       client.getOutStream().println(
-          userSender.toString() + "<span>: " + msg+"</span>");
+          userSender.toString() + " : "+msg);
     }
   }
 
-  // send list of clients to all Users
-  public void broadcastAllUsers(){
-    for (User client : this.clients) {
-      client.getOutStream().println(this.clients);
-    }
-  }
-
-  // send message to a User (String)
-  public void sendMessageToUser(String msg, User userSender, String user){
-    boolean find = false;
+  public void sendMessageToUser(String msg, User userSender, String user){;
     for (User client : this.clients) {
       if (client.getNickname().equals(user) && client != userSender) {
-        find = true;
         userSender.getOutStream().println(userSender.toString() + " -> " + client.toString() +": " + msg);
         client.getOutStream().println(
-            "(<b>Private</b>)" + userSender.toString() + "<span>: " + msg+"</span>");
+            "(<b>Private</b>)" + userSender.toString() + msg);
       }
-    }
-    if (!find) {
-      userSender.getOutStream().println(userSender.toString() + " -> (<b>no one!</b>): " + msg);
     }
   }
 }
@@ -107,7 +92,6 @@ class UserHandler implements Runnable {
   public UserHandler(Server server, User user) {
     this.server = server;
     this.user = user;
-    this.server.broadcastAllUsers();
   }
 
   public void run() {
@@ -117,18 +101,6 @@ class UserHandler implements Runnable {
     Scanner sc = new Scanner(this.user.getInputStream());
     while (sc.hasNextLine()) {
       message = sc.nextLine();
-
-      // smiley
-      message = message.replace(":)", "<img src='http://4.bp.blogspot.com/-ZgtYQpXq0Yo/UZEDl_PJLhI/AAAAAAAADnk/2pgkDG-nlGs/s1600/facebook-smiley-face-for-comments.png'>");
-      message = message.replace(":D", "<img src='http://2.bp.blogspot.com/-OsnLCK0vg6Y/UZD8pZha0NI/AAAAAAAADnY/sViYKsYof-w/s1600/big-smile-emoticon-for-facebook.png'>");
-      message = message.replace(":d", "<img src='http://2.bp.blogspot.com/-OsnLCK0vg6Y/UZD8pZha0NI/AAAAAAAADnY/sViYKsYof-w/s1600/big-smile-emoticon-for-facebook.png'>");
-      message = message.replace(":(", "<img src='http://2.bp.blogspot.com/-rnfZUujszZI/UZEFYJ269-I/AAAAAAAADnw/BbB-v_QWo1w/s1600/facebook-frown-emoticon.png'>");
-      message = message.replace("-_-", "<img src='http://3.bp.blogspot.com/-wn2wPLAukW8/U1vy7Ol5aEI/AAAAAAAAGq0/f7C6-otIDY0/s1600/squinting-emoticon.png'>");
-      message = message.replace(";)", "<img src='http://1.bp.blogspot.com/-lX5leyrnSb4/Tv5TjIVEKfI/AAAAAAAAAi0/GR6QxObL5kM/s400/wink%2Bemoticon.png'>");
-      message = message.replace(":P", "<img src='http://4.bp.blogspot.com/-bTF2qiAqvi0/UZCuIO7xbOI/AAAAAAAADnI/GVx0hhhmM40/s1600/facebook-tongue-out-emoticon.png'>");
-      message = message.replace(":p", "<img src='http://4.bp.blogspot.com/-bTF2qiAqvi0/UZCuIO7xbOI/AAAAAAAADnI/GVx0hhhmM40/s1600/facebook-tongue-out-emoticon.png'>");
-      message = message.replace(":o", "<img src='http://1.bp.blogspot.com/-MB8OSM9zcmM/TvitChHcRRI/AAAAAAAAAiE/kdA6RbnbzFU/s400/surprised%2Bemoticon.png'>");
-      message = message.replace(":O", "<img src='http://1.bp.blogspot.com/-MB8OSM9zcmM/TvitChHcRRI/AAAAAAAAAiE/kdA6RbnbzFU/s400/surprised%2Bemoticon.png'>");
 
       // Gestion des messages private
       if (message.charAt(0) == '@'){
@@ -145,9 +117,7 @@ class UserHandler implements Runnable {
 
       // Gestion du changement
       }else if (message.charAt(0) == '#'){
-        user.changeColor(message);
         // update color for all other users
-        this.server.broadcastAllUsers();
       }else{
         // update user list
         server.broadcastMessages(message, user);
@@ -155,7 +125,6 @@ class UserHandler implements Runnable {
     }
     // end of Thread
     server.removeUser(user);
-    this.server.broadcastAllUsers();
     sc.close();
   }
 }
@@ -176,28 +145,7 @@ class User {
     this.client = client;
     this.nickname = name;
     this.userId = nbUser;
-    this.color = ColorInt.getColor(this.userId);
     nbUser += 1;
-  }
-
-  // change color user
-  public void changeColor(String hexColor){
-    // check if it's a valid hexColor
-    Pattern colorPattern = Pattern.compile("#([0-9a-f]{3}|[0-9a-f]{6}|[0-9a-f]{8})");
-    Matcher m = colorPattern.matcher(hexColor);
-    if (m.matches()){
-      Color c = Color.decode(hexColor);
-      // if the Color is too Bright don't change
-      double luma = 0.2126 * c.getRed() + 0.7152 * c.getGreen() + 0.0722 * c.getBlue(); // per ITU-R BT.709
-      if (luma > 160) {
-        this.getOutStream().println("<b>Color Too Bright</b>");
-        return;
-      }
-      this.color = hexColor;
-      this.getOutStream().println("<b>Color changed successfully</b> " + this.toString());
-      return;
-    }
-    this.getOutStream().println("<b>Failed to change color</b>");
   }
 
   // getteur
@@ -216,29 +164,7 @@ class User {
   // print user with his color
   public String toString(){
 
-    return "<u><span style='color:"+ this.color
-      +"'>" + this.getNickname() + "</span></u>";
+    return this.getNickname();
 
   }
-}
-
-class ColorInt {
-    public static String[] mColors = {
-            "#3079ab", // dark blue
-            "#e15258", // red
-            "#f9845b", // orange
-            "#7d669e", // purple
-            "#53bbb4", // aqua
-            "#51b46d", // green
-            "#e0ab18", // mustard
-            "#f092b0", // pink
-            "#e8d174", // yellow
-            "#e39e54", // orange
-            "#d64d4d", // red
-            "#4d7358", // green
-    };
-
-    public static String getColor(int i) {
-        return mColors[i % mColors.length];
-    }
 }
